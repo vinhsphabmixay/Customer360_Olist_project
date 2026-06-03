9d8005883da1d9e0df421a7508ccd6e05eeab581
vinhsphabmixay <mistersvinh@gmail.com>
2026-06-03 12:05:21 +0200

    Adjustments

--- dbt/ecommerce_olist/models/marts/core/dim_is_new_customer.sql
+++ dbt/ecommerce_olist/models/marts/core/dim_is_new_customer.sql
@@ -2,7 +2,18 @@
 
 WITH DUMMY AS (
     SELECT
-        
+        CUSTOMER_SK
+        ,DATE
+        ,DATE_MONTH
+        ,ORDERS_COUNT
+        ,DAILY_REVENUE
+        ,CUMULATIVE_ORDERS_COUNT
+        ,CUMULATIVE_REVENUE
+        ,ORDER_NUMBER_FOR_CUSTOMER
+        ,CASE
+            WHEN ORDER_NUMBER_FOR_CUSTOMER = 1 THEN 1
+            ELSE 0
+        END AS IS_NEW_CUSTOMER_DAY
     FROM {{ref('fct_customer_daily_metrics')}}
 )
 
